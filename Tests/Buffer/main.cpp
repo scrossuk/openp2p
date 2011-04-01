0d0ccf81023c3af55b32dbd635917847e8c29b91
Stephen Cross <scross99@gmail.com>
2011-04-01 01:13:13 +0100

    Fixed and improved buffer test

--- Tests/Buffer/main.cpp
+++ Tests/Buffer/main.cpp
@@ -9,49 +9,23 @@
 
 using namespace OpenP2P;
 
-void TestDataReader(BinaryStream& reader){
-	for(std::size_t i = 0; i < 10; i++){
-		uint8_t v;
-		std::cout << "Try read" << std::endl;
-		if(!(reader >> v)){
-			std::cout << "Read failed" << std::endl;
-		}else{
-			std::cout << "Read successful: " << (unsigned int) v << std::endl;
-		}
-	}
-}
-
 int main(int argc, char *argv[]){
-	std::cout << "Build 0" << std::endl;
-	
 	BufferBuilder build0;
 	BinaryStream stream0(build0);
 	stream0 << int64_t(-4000);
-	
-	std::cout << "Build 1" << std::endl;
 	
 	BufferBuilder build1;
 	BinaryStream stream1(build1);
 	stream1 << uint32_t(42);
 	
-	std::cout << "Build 2" << std::endl;
-	
 	BufferBuilder build2;
 	BinaryStream stream2(build2);
 	stream2 << uint8_t(1);
-	
-	std::cout << build0.size() << ", " << build1.size() << ", " << build2.size() << std::endl;
-	
-	printBuffer(build0.getBuffer());
-	printBuffer(build1.getBuffer());
-	printBuffer(build2.getBuffer());
 
 	BufferQueue queue;
 	queue.add(build0.getBuffer());
 	queue.add(build1.getBuffer());
 	queue.add(build2.getBuffer());
-	
-	std::cout << "Added to queue" << std::endl;
 
 	int64_t a;
 	uint32_t b;
@@ -60,24 +34,34 @@ int main(int argc, char *argv[]){
 	BinaryStream queueStream(queue);
 	queueStream >> a >> b >> c;
 
-	std::cout << "Data = " << a << ", " << b << ", " << (unsigned int) c << std::endl;
-
-	TestDataReader(queue);
+	//All data in queue should have been consumed - so this should fail
+	uint8_t someData;
+	if(queueStream >> someData){
+		std::cout << "Test failed (" << __LINE__ << ") - Queue read gives more data than it should" << std::endl;
+		return 1;
+	}
 	
 	BufferBuilder builder;
 	BinaryStream builderStream(builder);
-	builderStream << a << b << c;
+	if(!(builderStream << a << b << c)){
+		std::cout << "Test failed (" << __LINE__ << ") - Failed to build buffer" << std::endl;
+		return 1;
+	}
 	
 	int64_t d;
 	uint32_t e;
 	uint8_t f;
 	
-	BufferIterator iterator(*builder);
+	BufferIterator iterator(builder.getBuffer());
 	BinaryStream iteratorStream(iterator);
-	if(iteratorStream >> d >> e >> f){
-		std::cout << "Data = " << d << ", " << e << ", " << (unsigned int) f << std::endl;
-	}else{
-		std::cout << "Couldn't read data" << std::endl;
+	if(!(iteratorStream >> d >> e >> f)){
+		std::cout << "Test failed (" << __LINE__ << ") - Failed to read from iterator" << std::endl;
+		return 1;
+	}
+
+	if(a != d || b != e || c != f){
+		std::cout << "Test failed (" << __LINE__ << ") - Data from iterator does not match data from building the buffer" << std::endl;
+		return 1;
 	}
 	
 	boost::array<TCP::Endpoint, 4> array;
@@ -89,22 +73,26 @@ int main(int argc, char *argv[]){
 	
 	BufferBuilder b2;
 	BinaryStream streamb2(b2);
-	if(streamb2 << array){
-		std::cout << "Successful array write" << std::endl;
+	if(!(streamb2 << array)){
+		std::cout << "Test failed (" << __LINE__ << ") - Failed to write to a boost::array" << std::endl;
+		return 1;
 	}
-	BufferIterator it2(*b2);
+
+	BufferIterator it2(b2.getBuffer());
 	BinaryStream streamit2(it2);
-	if(streamit2 >> array2){
-		std::cout << "Successful array read" << std::endl;
+	if(!(streamit2 >> array2)){
+		std::cout << "Test failed (" << __LINE__ << ") - Failed to read from a boost::array" << std::endl;
+		return 1;
 	}
 	
 	for(std::size_t i = 0; i < array.size(); i++){
-		std::cout << array[i].port() << std::endl;
+		if(array[i] != array2[i]){
+			std::cout << "Test failed (" << __LINE__ << ") - Arrays are not equal" << std::endl;
+			return 1;
+		}
 	}
-	
-	for(std::size_t i = 0; i < array2.size(); i++){
-		std::cout << array2[i].port() << std::endl;
-	}
+
+	std::cout << "Test passed" << std::endl;
 
 	return 0;
 }
